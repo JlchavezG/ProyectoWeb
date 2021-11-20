1cd693cfd073db7bbc8450730b97ffb8a7c08af5
IscJlchavezG <46545953+JlchavezG@users.noreply.github.com>
2021-11-20 10:11:06 -0600

    se actualiza index en proyecto web
    se coloco formulario de contacto y acordeon

--- index.php
+++ index.php
@@ -131,28 +131,204 @@
             <h2 class="text-center display-6 mt-3">Equipo de trabajo</h2>
              <div class="col">
                 <div class="card shadow mt-3">
-                     <h4 class="text-center">Samantha</h4>
+                     <div class="container mt-4">
+                        <img src="img/user_Mujer.png" alt="Imagen de perfil" style="width:200px;">
+                     </div>
+                     <h4 class="text-center text-primary mt-2">Samantha</h4>
+                     <div class="row mt-2">
+                          <div class="container text-center">
+                              <span class="">Ing. Sistemas</span>
+                          </div>
+                     </div>
+                     <div class="row mt-2">
+                          <div class="container text-center py-3">
+                            <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                               <use xlink:href="library/icons/bootstrap-icons.svg#facebook"/></a>
+                            </svg>
+                            <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                               <use xlink:href="library/icons/bootstrap-icons.svg#twitter"/>
+                            </svg></a>
+                            <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                               <use xlink:href="library/icons/bootstrap-icons.svg#instagram"/>
+                            </svg></a>
+                          </div>
+                     </div>
                 </div>
              </div>
              <div class="col">
                <div class="card shadow mt-3">
-                    <h4 class="text-center">Angel</h4>
+                 <div class="container mt-4">
+                    <img src="img/user_Mujer.png" alt="Imagen de perfil" style="width:200px;">
+                 </div>
+                 <h4 class="text-center text-primary mt-2">Samantha</h4>
+                 <div class="row mt-2">
+                      <div class="container text-center">
+                          <span class="">Ing. Sistemas</span>
+                      </div>
+                 </div>
+                 <div class="row mt-2">
+                      <div class="container text-center py-3">
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#facebook"/></a>
+                        </svg>
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#twitter"/>
+                        </svg></a>
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#instagram"/>
+                        </svg></a>
+                      </div>
+                 </div>
                </div>
              </div>
              <div class="col">
                <div class="card shadow mt-3">
-                    <h4 class="text-center">Cristopher</h4>
+                 <div class="container mt-4">
+                    <img src="img/user_Mujer.png" alt="Imagen de perfil" style="width:200px;">
+                 </div>
+                 <h4 class="text-center text-primary mt-2">Samantha</h4>
+                 <div class="row mt-2">
+                      <div class="container text-center">
+                          <span class="">Ing. Sistemas</span>
+                      </div>
+                 </div>
+                 <div class="row mt-2">
+                      <div class="container text-center py-3">
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#facebook"/></a>
+                        </svg>
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#twitter"/>
+                        </svg></a>
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#instagram"/>
+                        </svg></a>
+                      </div>
+                 </div>
                </div>
              </div>
              <div class="col">
                <div class="card shadow mt-3">
-                    <h4 class="text-center">Joshepth</h4>
+                 <div class="container mt-4">
+                    <img src="img/user_Mujer.png" alt="Imagen de perfil" style="width:200px;">
+                 </div>
+                 <h4 class="text-center text-primary mt-2">Samantha</h4>
+                 <div class="row mt-2">
+                      <div class="container text-center">
+                          <span class="">Ing. Sistemas</span>
+                      </div>
+                 </div>
+                 <div class="row mt-2">
+                      <div class="container text-center py-3">
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#facebook"/></a>
+                        </svg>
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#twitter"/>
+                        </svg></a>
+                        <a href="#"><svg class="bi" width="17" height="17" fill="currentColor">
+                           <use xlink:href="library/icons/bootstrap-icons.svg#instagram"/>
+                        </svg></a>
+                      </div>
+                 </div>
                </div>
              </div>
        </div>
      </div>
 </section>
 <!-- termina el team-->
+<!-- acordeon de servicios -->
+<section id="masservicios">
+    <div class="container text-center">
+        <h2 class="display-6"> + Servicios</h2>
+    </div>
+        <div class="container">
+          <div class="row py-3">
+             <!-- inicia mi acordeon -->
+             <div class="accordion accordion-flush" id="accordionServicios">
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingOne">
+                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                              Soporte Tecnico
+                         </button>
+                    </h2>
+                    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                       <div class="accordion-body"> texto de servicio de soporte tecnico.
+                         <div class="row mt-2">
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                               <img src="img/Card4.png" alt="" class="img-fluid">
+                            </div>
+                            <div class="col-sm-12 col-md-6 col-lg-6">
+                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                         </div>
+                       </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header" id="flush-headingTwo">
+                       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                          Diseño de páginas web
+                       </button>
+                    </h2>
+                      <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+                          <div class="accordion-body"> Texto para servico web.</div>
+                      </div>
+                </div>
+                <div class="accordion-item">
+                      <h2 class="accordion-header" id="flush-headingThree">
+                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                            Posicionamiento SEO
+                         </button>
+                       </h2>
+                       <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+                          <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                       </div>
+                 </div>
+              </div>
+             <!-- termina mi acoedeon -->
+        </div>
+    </div>
+</section>
+<section id="Contacto">
+    <div class="row mt-5">
+       <div class="col-sm-12 col-md-7 col-lg-7 py-2 bg-dark">
+           <h2 class="text-center display-6 text-light">Formulario de contacto</h2>
+           <div class="container justify-content-md-center py-2">
+             <form class="" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                <div class="row py-2">
+                   <div class="col">
+                      <input type="text" name="nombre" placeholder="Nombre" required class="form-control">
+                   </div>
+                </div>
+                <div class="row py-2">
+                   <div class="col">
+                      <input type="email" name="Email" placeholder="Correo Electronico" required class="form-control">
+                   </div>
+                </div>
+                <div class="row py-2">
+                   <div class="col">
+                      <input type="tel" name="Telefono" placeholder="Telefono" required class="form-control">
+                   </div>
+                </div>
+                <div class="row py-2">
+                   <div class="col">
+                     <textarea name="Mensaje" placeholder="Tu mensaje" class="form-control"></textarea>
+                   </div>
+                </div>
+                <div class="row py-2">
+                   <div class="col">
+                     <input type="submit" name="Mensaje" value="Mensaje" class="btn btn-sm btn-primary">
+                   </div>
+                </div>
+             </form>
+           </div>
+       </div>
+       <div class="col-sm-12 col-md-5 col-lg-5">
+           <div id="map"></div>
+       </div>
+    </div>
+</section>
   <script src="js/bootstrap.min.js"></script>
   </body>
 </html>
